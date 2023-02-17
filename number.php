f199057669a4a6d99e5fd73d67d58d70667c9a35
ruoitrau2014 <nganv1986@gmail.com>
2023-02-17 11:17:39 +0700

    Update number.php

--- number.php
+++ number.php
@@ -1,1 +1,17 @@
-
+<?php
+// số pi
+echo(pi()); // returns 3.1415926535898
+// min max
+echo(min(0, 150, 30, 20, -8, -200));  // returns -200
+echo(max(0, 150, 30, 20, -8, -200));  // returns 150
+// giá trị tuyệt đối
+echo(abs(-6.7));  // returns 6.7
+// căb bậc 2
+echo(sqrt(64));  // returns 8
+//làm tròn số nguyên
+echo(round(0.60));  // returns 1
+echo(round(0.49));  // returns 0
+// raNDON 
+echo(rand());
+echo(rand(10, 100));
+?>
